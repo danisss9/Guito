f4df7b59c696655664140b550733afdb29c304e6
Dani Santos <daniramossantos9@gmail.com>
2024-06-20 00:24:08 +0100

    MVP

---
+++ server/index.ts
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import fastifyCors from '@fastify/cors';
+import open from 'open';
+import simpleGit from 'simple-git';
+
+void (async function main() {
+  // Calculate dirname
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  // Get port number
+  const portArgIndex = process.argv.findIndex((a) => a.toLowerCase() === '--port');
+  const port = portArgIndex !== -1 ? +process.argv[portArgIndex + 1] : 8080;
+
+  // Get if should not open browser
+  const shouldOpenBrowser = process.argv.findIndex((a) => a.toLowerCase() === '--no-open') === -1;
+
+  // Initialize server
+  const app = fastify({
+    logger: false,
+  });
+
+  // Register cors
+  await app.register(fastifyCors);
+
+  // Register static file provider
+  app.register(fastifyStatic, {
+    root: join(__dirname, 'ui'),
+  });
+
+  // Serve UI
+  app.get('/', (_req, resp) => resp.sendFile('index.html'));
+
+  // Initialize git lib
+  const git = simpleGit(process.cwd());
+
+  // Git Api
+  app.get('/api/commits', (_req, resp) => {
+    git.log({}, (err, data) => {
+      resp.type('application/json').send(data.all.map((e) => e.message));
+    });
+  });
+
+  // Run the server
+  app.listen({ port }, (err, address) => {
+    if (err) throw err;
+
+    console.log(`Visit "${address}" to see git explorer.`);
+
+    if (shouldOpenBrowser) open(address);
+  });
+})();
---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let data: string[] = [];
+
+  onMount(async () => {
+    const resp = await fetch('http://localhost:8080/api/commits', { method: 'GET' });
+    data = await resp.json();
+    console.log({ data });
+  });
+</script>
+
+{#each data as commit}
+  <p>{commit}</p>
+{/each}
